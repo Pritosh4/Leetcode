c88b07c7d332dfe6064c5ac80565949d068c4813
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2022-08-25 16:35:16 +0530

    Update 75. Sort Colors.py

--- Medium/75. Sort Colors.py
+++ Medium/75. Sort Colors.py
@@ -3,18 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero, one, two = 0, 0, 0
-        for i in nums:
-            if i == 0:
-                zero += 1 #Counting the number of 0s
-            elif i == 1:
-                one += 1 #Counting the number of 1s
-            elif i == 2:
-                two += 1 #Counting the number of 2s
-        #modifying the list with the number of 0s, 1s, 2s that have been counted
-        for i in range(0,zero): 
-            nums[i] = 0
-        for i in range(zero, zero + one):
-            nums[i] = 1
-        for i in range(zero + one, zero + one + two):
-            nums[i] = 2
+        p0 = 0
+        p2 = len(nums)-1
+        i = 0
+        count = 0
+        while count < len(nums):
+            if nums[i] == 0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                i += 1
+                p0 += 1
+            elif nums[i] == 1:
+                i += 1
+            else:
+                nums[i], nums[p2] = nums[p2], nums[i]
+                p2 -= 1
+            count += 1
+        
