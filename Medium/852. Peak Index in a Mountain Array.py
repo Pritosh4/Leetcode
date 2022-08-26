85cc19b468f10604b0cf219be3a67580324373ce
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2022-08-26 21:49:07 +0530

    Create 852. Peak Index in a Mountain Array.py

---
+++ Medium/852. Peak Index in a Mountain Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        lo = 0
+        hi = len(arr) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if arr[mid] > arr[mid+1] and arr[mid] > arr[mid-1]:
+                return mid
+            elif arr[mid] < arr[mid+1]:
+                lo = mid + 1
+            elif arr[mid] > arr[mid+1]:
+                hi = mid - 1
+                
