761f146b6c03d8fdf1d71adffa4c6d7c814e938d
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2022-08-29 15:06:33 +0530

    Update 278. First Bad Version.py

--- Easy/278. First Bad Version.py
+++ Easy/278. First Bad Version.py
@@ -3,13 +3,15 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        lo = 1
-        hi = n
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if n == 1 or isBadVersion(mid) == True and isBadVersion(mid-1) == False:
+        low = 1
+        high = n
+        while low <= high:
+            mid = (low + high) // 2
+			# If n is 1 then we have to return that as the bad version else we check to see 
+			# if the current mid version is a bad version and the version before is not a bad version.
+            if n == 1 or isBadVersion(mid) == True and isBadVersion(mid-1) == False:  
                 return mid
-            if isBadVersion(mid) == True:
+            if isBadVersion(mid) == True: # If true then the first bad version lies on the left of mid
                 hi = mid - 1 
-            elif isBadVersion(mid) == False:
+            elif isBadVersion(mid) == False: # If false then the first bad version lies on the right of mid
                 lo = mid + 1
