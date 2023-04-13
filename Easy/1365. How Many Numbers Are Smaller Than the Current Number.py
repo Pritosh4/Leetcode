56780b0cc2409d38f85a7584234110baf9905cb8
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-04-14 00:39:27 +0530

    Create 1365. How Many Numbers Are Smaller Than the Current Number.py

---
+++ Easy/1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        temp = sorted(nums)
+        ans = []
+        mapping = {}
+        for i in range(len(temp)):
+            if temp[i] not in mapping:
+                mapping[temp[i]] = i
+        for i in range(len(nums)):
+            ans.append(mapping[nums[i]])
+        return ans
