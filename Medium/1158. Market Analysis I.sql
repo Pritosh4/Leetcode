c0a47bcff236c22cb7f9037f4ec2909955ff2178
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-04-17 23:46:02 +0530

    Create 1158. Market Analysis I.sql

---
+++ Medium/1158. Market Analysis I.sql
@@ -0,0 +1,5 @@
+SELECT user_id AS buyer_id, join_date, COUNT(order_id) AS orders_in_2019
+FROM Users AS u
+LEFT JOIN Orders AS o
+ON u.user_id = o.buyer_id AND YEAR(order_date) = '2019'
+GROUP BY user_id;
