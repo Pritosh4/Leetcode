46907feb34ba9414043f460f5b94aff513567e6a
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-04-23 00:27:26 +0530

    Create 1816. Truncate Sentence.py

---
+++ Easy/1816. Truncate Sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        count = 0
+        for i in range(len(s)):
+            if s[i] == ' ':
+                count += 1
+                if count == k:
+                    return s[:i]
+        return s
