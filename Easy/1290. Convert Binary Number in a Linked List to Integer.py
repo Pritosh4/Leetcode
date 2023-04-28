d038f7fecf8c5cdf9629e52d88ecfe565cb47d43
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-04-28 13:27:55 +0530

    Update 1290. Convert Binary Number in a Linked List to Integer.py

--- Easy/1290. Convert Binary Number in a Linked List to Integer.py
+++ Easy/1290. Convert Binary Number in a Linked List to Integer.py
@@ -17,3 +17,18 @@ class Solution:
                 ans = ans + mul
             mul *= 2
         return ans
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        ptr = head
+        ans = 0
+        while ptr:
+            #ans = ans * 2 + ptr.val 
+            ans = ans << 1 | ptr.val
+            ptr = ptr.next
+        return ans
