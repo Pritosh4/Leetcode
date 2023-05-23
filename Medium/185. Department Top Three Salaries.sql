6ca180741310178e305a418df4dd21964d5f3cc9
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-05-23 19:25:43 +0530

    Delete 185. Department Top Three Salaries.sql

--- Medium/185. Department Top Three Salaries.sql
+++
@@ -1,7 +0,0 @@
-SELECT D.name AS Department, E1.name as Employee, E1.salary AS Salary
-FROM Employee AS E1
-JOIN Department AS D
-ON E1.departmentId = D.id
-WHERE 3 > (SELECT COUNT(DISTINCT E2.salary)
-           FROM Employee AS E2
-           WHERE E2.salary > E1.salary AND E2.departmentId = E1.departmentId);
