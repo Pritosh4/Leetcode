03026788b2c43c61f799c9d1d2d6d92456372ed4
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-05-23 19:57:55 +0530

    Update 262. Trips and Users.sql

--- Hard/262. Trips and Users.sql
+++ Hard/262. Trips and Users.sql
@@ -1,4 +1,4 @@
-SELECT request_at AS Day, ROUND((COUNT(IF(status != 'completed', 1, NULL))/COUNT(status)), 2) AS 'Cancellation Rate'
+SELECT request_at AS Day, ROUND((COUNT(CASE WHEN status != 'completed' THEN 1 ELSE NULL END)/COUNT(status)), 2) AS 'Cancellation Rate'
 FROM Trips 
 WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
       AND client_id NOT IN (SELECT users_id 
@@ -7,4 +7,4 @@ WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
       AND driver_id NOT IN (SELECT users_id 
                             FROM Users
                             WHERE banned = 'Yes')
-GROUP BY request_at
+GROUP BY request_at;
