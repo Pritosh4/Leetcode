6b5171d4d38bf1fbc662e344ca3f80a7ff26ba5d
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-06-28 04:10:09 +0530

    Create 214. Shortest Palindrome.py

---
+++ Hard/214. Shortest Palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        rev_s = s[::-1] 
+        for i in range(len(s)):
+            if s[ : len(s) - i ] == rev_s[i:]:
+                return rev_s[: i] + s 
+        return ''
+        
