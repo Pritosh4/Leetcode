b138e71880afd66bdb0c5a0d64832e521f2098dc
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-07-05 06:40:34 +0530

    Create 1352. Product of the Last K Numbers.py

---
+++ Medium/1352. Product of the Last K Numbers.py
@@ -0,0 +1,28 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.data = []
+        self.product = 1
+
+    def add(self, num: int) -> None:
+        if num != 0:
+            self.product *= num
+            self.data.append(self.product)
+        else:
+            self.product = 1
+            self.data = []
+        
+
+    def getProduct(self, k: int) -> int:
+        if len(self.data) < k:
+            return 0
+        elif len(self.data) == k:
+            return self.data[-1]
+        return int(self.data[-1] / self.data[-1-k])
+
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
